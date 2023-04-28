c276727a459ca9d7eac18a1b752f19425d7ceeb0
Patrick Maweu <60652548+muilipato@users.noreply.github.com>
2023-04-28 15:39:02 +0300

    Added directors

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,31 @@
+
 import React from "react";
 import { directors } from "../data";
 
+const directorsList = directors.map(director => {
+  return (
+    <div className="directorCard" key={director.name}>
+      <p>{director.name}</p>
+      <div>Filmography:
+        <ul>
+          {director.movies.map(movie => {
+            return (
+              <li key={movie}>{movie}</li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+})
+
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsList}
+    </div>
+  )
 }
 
 export default Directors;

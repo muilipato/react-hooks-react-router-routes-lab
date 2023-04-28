2532fd8647e86d4f2f8dfbe1f9e9b94dd99242b7
Patrick Maweu <60652548+muilipato@users.noreply.github.com>
2023-04-28 15:42:08 +0300

    Added movies

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { movies } from "../data";
 
+const movieList = movies.map(movie => {
+  return (
+    <div className="movieCard" key={movie.title}>
+      <p>{movie.title}</p>
+      <p>Time: {movie.time} minutes</p>
+      <div>Genres:
+        <ul>
+          {movie.genres.map(genre => {
+            return (
+              <li key={genre}>{genre}</li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+})
+
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movieList}
+    </div>
+  )
 }
 
 export default Movies;
+
+
+
+  
